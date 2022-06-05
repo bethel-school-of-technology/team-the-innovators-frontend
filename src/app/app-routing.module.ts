132ca4e6ce177c3f46de2a7f65abe41f45abeba1
Noah Braswell <noahbraz23@gmail.com>
2022-06-04 22:07:46 -0400

    base home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "hotspots", component: HomeComponent},
+  { path: "signup", component: SignupComponent},
+  { path: "", redirectTo: "hotspots", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ users.ts
@@ -0,0 +1,7 @@
+export class Contact {
+    id!: number;
+    first_name!: string;
+    last_name!: string;
+    username!: string;
+    password!: string;
+}
