973260546783da2bcb1655f87413d74dd335a521
Bryana Aleman <alemanb2018@gmail.com>
2022-06-08 13:42:46 -0500

    updated app routing to include account route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   // localhost:4200/login
-  { path: "login", component: LoginComponent }
+  { path: "login", component: LoginComponent },
+  // localhost:4200/account/1
+  { path: "account/:id", component: AccountComponent }
 ];
 
 @NgModule({
