4d7315d9b3ae888558db9221048d200a3108436a
Bryana Aleman <alemanb2018@gmail.com>
2022-06-17 09:43:14 -0500

    created navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AccountComponent } from './components/account/account.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AccountComponent
+    AccountComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
