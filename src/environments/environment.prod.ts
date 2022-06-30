4feef857be5248fafd574c6b5c18a352b5ab0962
Noah Braswell <noahbraz23@gmail.com>
2022-06-30 18:23:48 -0400

    code that we had before errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  accessToken: 'pk.eyJ1IjoiYmFsZW1hbjk1IiwiYSI6ImNsNGl3OHhhdjAwZDgzZHBpeDI5bXExYWwifQ.CHC73_NUxgmD7dUglPiNhQ'
 };
